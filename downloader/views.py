4d155d5e27b89be08ccf533a808f03186f589aab
Marcio Silva <marciosilvadrum@gmail.com>
2024-06-12 14:45:52 -0300

    adding dotenv

--- downloader/views.py
+++ downloader/views.py
@@ -5,11 +5,14 @@ from .forms import YouTubeLinkForm
 from pytube import YouTube
 import threading
 import os
+from pathlib import Path
 from django.contrib import messages
-from django.conf import settings
 
 def download_complete(request):
-    downloads_path = settings.DOWNLOADS_PATH
+    # Determine the path to the Downloads directory
+    home = str(Path.home())
+    downloads_path = os.path.join(home, 'Downloads')
+    
     return render(request, "final.html", {'downloads_path': downloads_path})
 
 def download_video_view(request):
@@ -23,23 +26,18 @@ def download_video_view(request):
                 # Getting the highest resolution
                 yd = yt.streams.get_highest_resolution()
                 
-                # Function to download the video
+                # Function to improve the download time
                 def download(): 
-                    downloads_path = settings.DOWNLOADS_PATH
-                    
-                    # Cria o diretório se não existir
-                    # if not os.path.exists(downloads_path):
-                    #     os.makedirs(downloads_path)
-                    
-                    # Faz o download do vídeo
+                    # Determine the path to the Downloads directory
+                    home = str(Path.home())
+                    downloads_path = os.path.join(home, 'Downloads')
                     yd.download(output_path=downloads_path)
-                    
-                    # Adiciona uma mensagem de sucesso
                     messages.success(request, f"Download Concluído! Vídeo salvo em {downloads_path}")
 
                 download_thread = threading.Thread(target=download)
                 download_thread.start()
 
+                
                 return redirect('download_complete')
 
             except Exception as e:
@@ -50,24 +48,16 @@ def download_video_view(request):
 
     return render(request, 'download.html', {'form': form})
 
-
-
-
-
-
 # from django.shortcuts import render, redirect
 # from .forms import YouTubeLinkForm
 # from pytube import YouTube
 # import threading
 # import os
-# from pathlib import Path
 # from django.contrib import messages
+# from django.conf import settings
 
 # def download_complete(request):
-#     # Determine the path to the Downloads directory
-#     home = str(Path.home())
-#     downloads_path = os.path.join(home, 'Downloads')
-    
+#     downloads_path = settings.DOWNLOADS_PATH
 #     return render(request, "final.html", {'downloads_path': downloads_path})
 
 # def download_video_view(request):
@@ -81,18 +71,23 @@ def download_video_view(request):
 #                 # Getting the highest resolution
 #                 yd = yt.streams.get_highest_resolution()
                 
-#                 # Function to improve the download time
+#                 # Function to download the video
 #                 def download(): 
-#                     # Determine the path to the Downloads directory
-#                     home = str(Path.home())
-#                     downloads_path = os.path.join(home, 'Downloads')
+#                     downloads_path = settings.DOWNLOADS_PATH
+                    
+#                     # Cria o diretório se não existir
+#                     if not os.path.exists(downloads_path):
+#                         os.makedirs(downloads_path)
+                    
+#                     # Faz o download do vídeo
 #                     yd.download(output_path=downloads_path)
+                    
+#                     # Adiciona uma mensagem de sucesso
 #                     messages.success(request, f"Download Concluído! Vídeo salvo em {downloads_path}")
 
 #                 download_thread = threading.Thread(target=download)
 #                 download_thread.start()
 
-                
 #                 return redirect('download_complete')
 
 #             except Exception as e:
@@ -102,4 +97,11 @@ def download_video_view(request):
 #         form = YouTubeLinkForm()
 
 #     return render(request, 'download.html', {'form': form})
+
+
+
+
+
+
+
 
