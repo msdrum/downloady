39c0d534eef257f151cd7523569f5095ffeed5c2
Marcio Silva <marciosilvadrum@gmail.com>
2024-06-12 16:27:50 -0300

    returning for old point

--- downloady/urls.py
+++ downloady/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-# from downloader.views import download_video_view, download_complete
-from downloader.views import download_video_view, file_iterator
+from downloader.views import download_video_view, download_complete
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', download_video_view, name='download_video'),
-    # path('complete/', download_complete, name='download_complete'),
-    path('complete/', file_iterator, name='download_complete'),
+    path('complete/', download_complete, name='download_complete'),
 ]
