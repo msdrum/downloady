7953e4f8ead53c8d508f82b72db947285be6ebb3
Marcio Silva <marciosilvadrum@gmail.com>
2024-06-12 15:58:19 -0300

    adding temp folder for downloads

--- downloader/views.py
+++ downloader/views.py
@@ -3,17 +3,19 @@
 from django.shortcuts import render, redirect
 from .forms import YouTubeLinkForm
 from pytube import YouTube
-import threading
 import os
-from pathlib import Path
+from django.http import StreamingHttpResponse
 from django.contrib import messages
+import tempfile
 
-def download_complete(request):
-    # Determine the path to the Downloads directory
-    home = str(Path.home())
-    downloads_path = os.path.join(home, 'Downloads')
-    
-    return render(request, "final.html", {'downloads_path': downloads_path})
+# Function to send the file as a streaming response
+def file_iterator(file_name, chunk_size=8192):
+    with open(file_name, 'rb') as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            yield chunk
 
 def download_video_view(request):
     if request.method == 'POST':
@@ -21,24 +23,30 @@ def download_video_view(request):
         if form.is_valid():
             link = form.cleaned_data['link']
             yt = YouTube(link)
-            
+
             try:
                 # Getting the highest resolution
                 yd = yt.streams.get_highest_resolution()
-                
-                # Function to improve the download time
-                def download(): 
-                    # Determine the path to the Downloads directory
-                    home = str(Path.home())
-                    downloads_path = os.path.join(home, 'Downloads')
-                    yd.download(output_path=downloads_path)
-                    messages.success(request, f"Download Concluído! Vídeo salvo em {downloads_path}")
 
-                download_thread = threading.Thread(target=download)
-                download_thread.start()
+                # Define the temporary directory path
+                temp_dir = tempfile.gettempdir()
 
-                
-                return redirect('download_complete')
+                # Check if the directory exists, if not, create it
+                if not os.path.exists(temp_dir):
+                    os.makedirs(temp_dir)
+
+                # Temporary file path
+                temp_file_path = os.path.join(temp_dir, yd.default_filename)
+                yd.download(output_path=temp_dir)
+
+                response = StreamingHttpResponse(file_iterator(temp_file_path))
+                response['Content-Type'] = 'video/mp4'
+                response['Content-Disposition'] = f'attachment; filename="{yd.default_filename}"'
+
+                # Optionally, you can remove the temporary file after sending it
+                # os.remove(temp_file_path)
+
+                return response
 
             except Exception as e:
                 messages.error(request, f"Ocorreu um erro: {e}")
@@ -48,16 +56,22 @@ def download_video_view(request):
 
     return render(request, 'download.html', {'form': form})
 
+
+
+
 # from django.shortcuts import render, redirect
 # from .forms import YouTubeLinkForm
 # from pytube import YouTube
 # import threading
 # import os
+# from pathlib import Path
 # from django.contrib import messages
-# from django.conf import settings
 
 # def download_complete(request):
-#     downloads_path = settings.DOWNLOADS_PATH
+#     # Determine the path to the Downloads directory
+#     home = str(Path.home())
+#     downloads_path = os.path.join(home, 'Downloads')
+    
 #     return render(request, "final.html", {'downloads_path': downloads_path})
 
 # def download_video_view(request):
@@ -71,23 +85,18 @@ def download_video_view(request):
 #                 # Getting the highest resolution
 #                 yd = yt.streams.get_highest_resolution()
                 
-#                 # Function to download the video
+#                 # Function to improve the download time
 #                 def download(): 
-#                     downloads_path = settings.DOWNLOADS_PATH
-                    
-#                     # Cria o diretório se não existir
-#                     if not os.path.exists(downloads_path):
-#                         os.makedirs(downloads_path)
-                    
-#                     # Faz o download do vídeo
+#                     # Determine the path to the Downloads directory
+#                     home = str(Path.home())
+#                     downloads_path = os.path.join(home, 'Downloads')
 #                     yd.download(output_path=downloads_path)
-                    
-#                     # Adiciona uma mensagem de sucesso
 #                     messages.success(request, f"Download Concluído! Vídeo salvo em {downloads_path}")
 
 #                 download_thread = threading.Thread(target=download)
 #                 download_thread.start()
 
+                
 #                 return redirect('download_complete')
 
 #             except Exception as e:
@@ -97,11 +106,3 @@ def download_video_view(request):
 #         form = YouTubeLinkForm()
 
 #     return render(request, 'download.html', {'form': form})
-
-
-
-
-
-
-
-
--- downloady/urls.py
+++ downloady/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from downloader.views import download_video_view, download_complete
+# from downloader.views import download_video_view, download_complete
+from downloader.views import download_video_view, file_iterator
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', download_video_view, name='download_video'),
-    path('complete/', download_complete, name='download_complete'),
+    # path('complete/', download_complete, name='download_complete'),
+    path('complete/', file_iterator, name='download_complete'),
 ]
